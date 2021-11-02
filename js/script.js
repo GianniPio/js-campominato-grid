98bf4d2d26566fe58eeb89db4084b905449f4fc7
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-02 16:24:30 +0100

    logical steps

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@
 // con difficoltà 2 => tra 1 e 81
 // con difficoltà 3 => tra 1 e 49
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
+
+// 1. Far scegliere all'utente la difficoltà -> if
+    // 1.1 Deve scegliere tra 3 numeri, in base al numero scelto, decide la difficoltà
+
+// 2. Creare le funzioni per la creazione della griglia
+    // 2.1 In base al numero scelto, si attiva una funzione diversa -> if
+
+
+// 3. In base alla difficoltà scelta, viene creare la griglia
+    // 3.1 Difficoltà 1 = 10X10
+    // 3.2 Difficoltà 2 = 9X9
+    // 3.3 Difficoltà 3 = 7X7
+
+// 4. Generare numeri -> for
